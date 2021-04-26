f175d3d9fb091f01736dff120d3b5a72070ecb4c
Gaston Tomas Huete <44206933+GastonTH@users.noreply.github.com>
2021-04-26 19:49:31 +0200

    First version
    first try

--- RPGConsole/RPGConsole/Program.cs
+++ RPGConsole/RPGConsole/Program.cs
@@ -6,7 +6,72 @@ namespace RPGConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool condicionWin = true;
+            bool whoWin = true; //true = heroe - false = villano
+            int turno = 0;
+
+            int vidaHeroe = 1000;
+            int vidaVillano = 1000;
+            int ataqueHeroe = 233;
+            int ataqueVillano = 121;
+
+            void esperarRespuesta()
+            {
+                Console.WriteLine("Esperando respuesta...");
+                Console.ReadLine();
+            }
+
+            //pequeño pvp
+
+            while (condicionWin)
+            {
+                //sistema de turnos
+
+                Console.WriteLine("Turno: " + turno);
+                Console.WriteLine("Turno del heroe");
+                Console.WriteLine("");
+
+                Console.WriteLine("El Heroe ataca al Villano y le quita " + ataqueHeroe);
+
+                vidaVillano -= ataqueHeroe;
+
+                Console.WriteLine("");
+
+                if (vidaVillano <= 0)
+                {
+                    condicionWin = false;
+                    whoWin = true;
+                    break;
+                }
+
+                Console.WriteLine("Vida del Villano: " + );
+                Console.WriteLine("El Villano ataca al Heroe y le quita " + ataqueVillano);
+
+                vidaHeroe -= ataqueVillano;
+
+                if (vidaHeroe <= 0)
+                {
+                    condicionWin = false;
+                    whoWin = false;
+                    break;
+                }
+
+
+                //-----------------
+                turno++;
+
+            }
+
+            if (whoWin)
+            {
+                Console.WriteLine("¡Ha ganado el Heroe!");
+            }
+            else
+            {
+                Console.WriteLine("Ha ganado el villano...");
+            }
+
+            esperarRespuesta();
         }
     }
 }
